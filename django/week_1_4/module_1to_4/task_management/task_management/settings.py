4b50f54531b6f21f3fe545be46a19ae9052f3039
zishan344 <zishanahmed344@gmail.com>
2025-01-19 17:39:56 +0600

    finish install django dibuggar

--- django/week_1_4/module_1to_4/task_management/task_management/settings.py
+++ django/week_1_4/module_1to_4/task_management/task_management/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
-    'users'
+    'users',
+    "debug_toolbar"
+
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 ROOT_URLCONF = 'task_management.urls'
--- django/week_1_4/module_1to_4/task_management/task_management/urls.py
+++ django/week_1_4/module_1to_4/task_management/task_management/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from tasks.views import home
-
+from debug_toolbar.toolbar import debug_toolbar_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home),
     path('tasks/',include('tasks.urls'))
-]
+] + debug_toolbar_urls()
