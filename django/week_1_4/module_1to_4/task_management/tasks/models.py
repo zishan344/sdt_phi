5e403716e4e74a08120c9166fb4397f6428afa04
zishan344 <zishanahmed344@gmail.com>
2025-01-21 18:46:01 +0600

    complete module 8.1

--- django/week_1_4/module_1to_4/task_management/tasks/models.py
+++ django/week_1_4/module_1to_4/task_management/tasks/models.py
@@ -1,55 +1,67 @@
 from django.db import models
+
+
 class Employee(models.Model):
-  name = models.CharField(max_length=100)
-  email = models.EmailField(unique=True)
-  def __str__(self):
-    return self.name
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
-  STATUS_CHOICES = [
-    ('PENDING', 'PENDING'),
-    ('IN_PROGRESS', 'IN_PROGRESS'),
-    ('COMPLETED', 'COMPLETED')
-  ]
-  project = models.ForeignKey("Project", on_delete = models.CASCADE,default=1)
-  assigned_to = models.ManyToManyField(Employee,related_name='tasks')
-  title = models.CharField(max_length=250)
-  description = models.TextField()
-  due_date = models.DateField()
-  status = models.CharField(max_length=15,choices = STATUS_CHOICES,default ="PENDING")
-  is_complete = models.BooleanField(default=False)
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed')
+    ]
+    project = models.ForeignKey(
+        "Project",
+        on_delete=models.CASCADE,
+        default=1
+    )
+    assigned_to = models.ManyToManyField(Employee, related_name='tasks')
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(
+        max_length=15, choices=STATUS_CHOICES, default="PENDING")
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # details
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 
 class TaskDetail(models.Model):
-  HIGH ='H'
-  MEDIUM ='M'
-  LOW ='L'
-  PRIORITY_CHOICES = (
-    (HIGH,'High'),
-    (MEDIUM,'Medium'),
-    (LOW,'Low'),
-  )
-  std_id = models.CharField(max_length=200,primary_key=True)
-  task = models.OneToOneField(Task,on_delete=models.CASCADE,related_name='details')
-  assigned_to = models.CharField(max_length=100)
-  priority = models.CharField(
-    max_length = 1, choices =PRIORITY_CHOICES, default = LOW
+    HIGH = 'H'
+    MEDIUM = 'M'
+    LOW = 'L'
+    PRIORITY_OPTIONS = (
+        (HIGH, 'High'),
+        (MEDIUM, 'Medium'),
+        (LOW, 'Low')
     )
-  notes = models.TextField(blank=True, null=True)
-  def __str__(self):
-    return f"Details form Task {self.task.title}"
+    task = models.OneToOneField(
+        Task,
+        on_delete=models.CASCADE,
+        related_name='details',
+    )
+    assigned_to = models.CharField(max_length=100)
+    priority = models.CharField(
+        max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+    notes = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return f"Details form Task {self.task.title}"
 
 
 class Project(models.Model):
-  name = models.CharField(max_length=255)
-  description = models.TextField()
-  start_date = models.DateField()
-  def __str__(self):
-    return self.name
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField()
+
+    def __str__(self):
+        return self.name
--- django/week_1_4/module_1to_4/task_management/tasks/views.py
+++ django/week_1_4/module_1to_4/task_management/tasks/views.py
@@ -12,7 +12,26 @@ def home(request):
 def user_dashboard(request):
   return render(request, 'dashboard/user_dashboard.html') """
 def manager_dashboard(request):
-    return render(request, "dashboard/manager-dashboard.html")
+  tasks = Task.objects.all()
+  
+  # # getting task count
+  total_task =tasks.count()
+  completed_task = Task.objects.filter(status="COMPLETED").count()
+  in_progress_task = Task.objects.filter(status="IN_PROGRESS").count()
+  pending_task = Task.objects.filter(status="PENDING").count()
+
+  counts = {
+    "tasks":tasks,
+    "total": total_task,
+    "completed": completed_task,
+    "in_progress": in_progress_task,
+    "pending": pending_task
+  }
+  context= {
+    "tasks": tasks,
+    "counts": counts
+  }
+  return render(request, "dashboard/manager-dashboard.html",context)
 
 
 def user_dashboard(request):
