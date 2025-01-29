4768c145ddf343ff77a7a16a21186ab3d3a52ec8
zishan344 <zishanahmed344@gmail.com>
2025-01-29 16:35:36 +0600

    complete module 10.4

--- django/week_1_4/module_1to_4/task_management/users/form.py
+++ django/week_1_4/module_1to_4/task_management/users/form.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
+import re
+from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
   class Meta:
@@ -10,3 +13,23 @@ class RegisterForm(UserCreationForm):
     for fieldname in ['username', 'password1', 'password2']:
       self.fields[fieldname].help_text = None
   
+
+class CustomRegistrationForm(forms.ModelForm):
+  class Meta:
+    fields = [
+      'username','first_name','last_name','password1','confirm_password','email'
+    ]
+  def clean_password1(self):
+    password1 = self.cleaned_data.get('password1')
+    # check for minimum length
+    if len(password1) < 8:
+      raise forms.ValidationError(
+        "Password must be at least 8 characters long"
+      )
+    # check for complexity (uppercase, lowercase, number, special characters)
+    elif not re.fullmatch(r"[A-Za-z0-9@#$%^&+=]{8,}",password1):
+      raise forms.ValidationError(
+        'Password must include uppercase, lowercase,number, and special character'
+      )
+    return password1
+
