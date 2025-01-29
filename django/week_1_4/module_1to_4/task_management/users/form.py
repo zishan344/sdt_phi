7734bbf1a94a560d30e409007352cad4a204a0c8
zishan344 <zishanahmed344@gmail.com>
2025-01-29 16:16:02 +0600

    complete module 10.3

---
+++ django/week_1_4/module_1to_4/task_management/users/form.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+  class Meta:
+    model = User
+    fields =['username', 'first_name','last_name', 'password1', 'password2', 'email']
+  def __init__(self, *args, **kwargs):
+    super(UserCreationForm, self).__init__(*args, **kwargs)
+    for fieldname in ['username', 'password1', 'password2']:
+      self.fields[fieldname].help_text = None
+  
--- django/week_1_4/module_1to_4/task_management/users/views.py
+++ django/week_1_4/module_1to_4/task_management/users/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from users.form import RegisterForm
+from django.contrib.auth.models import User
 
 def sign_up(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
-            form.save()
+          # username = form.cleaned_data.get('username')
+          # password = form.cleaned_data.get('password1')
+          # confirm_password = form.cleaned_data.get('password2')
+          
+          # if password == confirm_password:
+          #   User.objects.create(username=username, password=password)
+          # else:
+          #   print("Password are not same")
+          form.save()
+        else:
+          print("Form is not valid")
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
 
     return render(request, 'registration/register.html', {"form": form})
