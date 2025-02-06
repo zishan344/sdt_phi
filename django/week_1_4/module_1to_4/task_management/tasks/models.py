57c9bdb2ca19b8e669d3fe81aa6ee40f6cd355a1
zishan344 <zishanahmed344@gmail.com>
2025-02-06 11:29:37 +0600

    completed module 13.4

--- django/week_1_4/module_1to_4/task_management/tasks/models.py
+++ django/week_1_4/module_1to_4/task_management/tasks/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
@@ -20,13 +20,13 @@ class Task(models.Model):
         on_delete=models.CASCADE,
         default=1
     )
-    assigned_to = models.ManyToManyField(Employee, related_name='tasks')
+    # assigned_to = models.ManyToManyField(Employee, related_name='tasks')
+    assigned_to = models.ManyToManyField(User, related_name='tasks')
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
     status = models.CharField(
         max_length=15, choices=STATUS_CHOICES, default="PENDING")
-    is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # details
