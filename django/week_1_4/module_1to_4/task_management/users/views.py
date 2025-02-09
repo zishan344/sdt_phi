06993fce7beee276defec5dd011f9f66f58cffe8
zishan344 <zishanahmed344@gmail.com>
2025-02-09 20:52:30 +0600

    complete moudule 16

--- django/week_1_4/module_1to_4/task_management/users/views.py
+++ django/week_1_4/module_1to_4/task_management/users/views.py
@@ -74,6 +74,7 @@ class CustomPasswordResetView(PasswordResetView):
   form_class = CustomPasswordResetForm
   template_name = 'registration/reset_password.html'
   success_url = reverse_lazy('sign-in')
+  html_email_template_name = 'registration/reset_email.html'
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
     context['protocol']='https'if self.request.is_secure() else 'http'
