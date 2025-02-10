0de6342bcd4561d60918d6d4ea25bf9eb06fd0ee
zishan344 <zishanahmed344@gmail.com>
2025-02-10 15:40:27 +0600

    complete module 17.6

--- django/week_1_4/module_1to_4/task_management/users/signals.py
+++ django/week_1_4/module_1to_4/task_management/users/signals.py
@@ -1,9 +1,12 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User,Group
+from django.contrib.auth.models import Group
 from django.contrib.auth.tokens import default_token_generator
 from django.conf import settings
 from django.core.mail import send_mail
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 @receiver(post_save,sender=User)
 def send_activation_email(sender,instance,created,**kwarg):
