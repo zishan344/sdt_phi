60a4c28204fe27673bb938b899e4e1bc1edb182c
zishan344 <zishanahmed344@gmail.com>
2025-02-10 12:16:34 +0600

    complete module 17.2

--- django/week_1_4/module_1to_4/task_management/users/urls.py
+++ django/week_1_4/module_1to_4/task_management/users/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('admin/<int:user_id>/assign-role/',assign_role,name='assign-role'),
     path('admin/create-group/',create_group,name='create-group'),
     path('admin/group-list/',group_list, name='group-list'),
-    path("profile/", ProfileView.as_view(), name=""),
+    path("profile/", ProfileView.as_view(), name="profile"),
     path("password-change/", ChangePassword.as_view(), name="password-change"),
     path("password-change/done/", PasswordChangeDoneView.as_view(template_name="accounts/password_change_done.html"), name="password_change_done"),
     path("password-reset/", CustomPasswordResetView.as_view(), name="password_reset"),
