fcee60fee442fafad3c29fb81870a453a3335fc2
zishan344 <zishanahmed344@gmail.com>
2025-02-24 10:52:08 +0600

    complete module 20.5

--- django/module_19_20/PhiMart/product/serializers.py
+++ django/module_19_20/PhiMart/product/serializers.py
@@ -3,12 +3,12 @@ from decimal import Decimal
 from product.models import Product, Category
 
 
-class CategorySerializer(serializers.Serializer):
+""" class CategorySerializer(serializers.Serializer):
   id = serializers.IntegerField()
   name = serializers.CharField()
   description = serializers.CharField()
-
-class ProductSerializer(serializers.Serializer):
+"""
+""" class ProductSerializer(serializers.Serializer):
   id = serializers.IntegerField()
   name = serializers.CharField()
   unit_price = serializers.DecimalField(decimal_places=2, max_digits=10, source = 'price')
@@ -16,4 +16,19 @@ class ProductSerializer(serializers.Serializer):
   category = CategorySerializer()
   def calculate_tax(self, product):
     return round(product.price * Decimal(1.1),2)
-  
+"""
+class CategorySerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Category
+    fields = ['id','name','description','product_count']
+  product_count = serializers.IntegerField()
+
+class ProductSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Product
+    fields = ['id','name','description','price','stock','category','price_with_tax']
+  price_with_tax = serializers.SerializerMethodField(method_name ="calculate_tax")
+  category = CategorySerializer()
+  def calculate_tax(self, product):
+    return round(product.price * Decimal(1.1),2)
+
--- django/module_19_20/PhiMart/product/views.py
+++ django/module_19_20/PhiMart/product/views.py
@@ -5,12 +5,24 @@ from rest_framework.response import Response
 from rest_framework import status
 from product.models import Category, Product
 from django.shortcuts import get_object_or_404
-from product.serializers import ProductSerializer
+from product.serializers import ProductSerializer, CategorySerializer
+from django.db.models import Count
 
 # Create your views here.
+@api_view()
+def view_categories(request):
+  categories = Category.objects.annotate(
+    product_count = Count('products')).all()
+  serializer = CategorySerializer(categories, many=True)
+  return Response(serializer.data)
 
-def view_specific_category(request):
-  pass
+@api_view()
+def view_specific_category(request, pk):
+  category = Category.objects.annotate(
+    product_count=Count('products')
+  ).get(pk=pk)
+  serializer = CategorySerializer(category)
+  return Response(serializer.data)
 
 @api_view()
 def view_products(request):
