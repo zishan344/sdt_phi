e3ce3f6f9165a4578c1184df06bc68802d6f2e87
zishan344 <zishanahmed344@gmail.com>
2025-02-25 15:57:34 +0600

    cng

--- django/module_19_20/PhiMart/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('products/',include('product.product_urls')),
-    path('categories/',include('product.category_urls'))
-]
--- django/module_19_20/PhiMart/product/category_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from product import views
-
-urlpatterns = [
-  path('', views.view_categories, name = 'category-list'),
-  path('<int:pk>/', views.view_specific_category, name = 'view-specific-category')
-]
