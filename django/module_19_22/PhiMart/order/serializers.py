541bdaccb7ebf31240efaaea246f8f1c8c1525cf
zishan344 <zishanahmed344@gmail.com>
2025-03-01 19:29:05 +0600

    complete module 22.3

---
+++ django/module_19_22/PhiMart/order/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from product.models import Product
+from order.models import Cart, CartItem
+from product.serializers import ProductSerializer
+class CartSerializer(serializers.ModelSerializer):
+  class Met:
+    model=Cart
+    field =['id', 'user']
+
+class SimpleProductSerializer(serializers.ModelSerializer):
+  class meta:
+    model = Product
+    fields = ['id','name','price']
+
+class CartItemSerializer(serializers.ModelSerializer):
+  product = SimpleProductSerializer()
+  total_price = serializers.SerializerMethodField(method_name='get_total_price')
+  class Meta:
+    mode = CartItem
+    fields= ["id",'product',
+            'quantity','total_price']
+  def get_total_price(self,cart_item:CartItem):
+    return cart_item.quantity * cart_item.product.price
+  
+class CartSerializer(serializers.ModelSerializer):
+  items = CartItemSerializer(many=True)
+  total_price = serializers.SerializerMethodField(method_name = "get_total_price")
+  class Meta:
+    model = Cart
+    fields = ['id','user','items','total_price']
+  def get_total_price(self,cart:Cart):
+    return sum([item.product.price * item.quantity for item in cart.items.all()])
+
+
