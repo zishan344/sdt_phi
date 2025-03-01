6f92aa95b6766cdd7b2ec1810e0f6b4f7c6dea8a
zishan344 <zishanahmed344@gmail.com>
2025-03-01 17:15:29 +0600

    complete 22.2

--- django/module_19_22/PhiMart/api/urls.py
+++ django/module_19_22/PhiMart/api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet
-
 from rest_framework_nested import routers
+from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet
+from order.views import CartViewSet
 
 router = DefaultRouter()
 router.register('products', ProductViewSet, basename='products')
 router.register('categories', CategoryViewSet, basename='category')
+
+router.register('carts',CartViewSet, basename="carts")
+
 
 product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 
--- django/module_19_22/PhiMart/order/views.py
+++ django/module_19_22/PhiMart/order/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.viewsets import GenericViewSet
+from order.serializers import CartSerializer
+from order.models import Cart
 # Create your views here.
+
+
+class CartViewSet(CreateModelMixin,RetrieveModelMixin,GenericViewSet):
+  queryset = Cart.objects.all()
+  serializer_class = CartSerializer
