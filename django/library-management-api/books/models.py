1381a7ace8783f135a56c74d0242beca94df5abf
zishan344 <zishanahmed344@gmail.com>
2025-03-16 15:02:16 +0600

    complete all book model

--- django/library-management-api/books/models.py
+++ django/library-management-api/books/models.py
@@ -1,3 +1,45 @@
 from django.db import models
-
+import uuid
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your models here.
+""" 
+model List on books app
+1. Category
+2. Book
+3. Author
+4. BorrowRecord
+"""
+
+class Author(models.Model):
+  """ This is the author model of book  """
+  id = models.UUIDField(primary_key=True,default=uuid.uuid4(),editable=False)
+  name = models.CharField(max_length=50)
+  biography = models.TextField(null=True, blank=True)
+  birth_date = models.DateField()
+class Category(models.Model):
+  """ This is book category Model"""
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+  name = models.CharField(max_length=50)
+  description = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True, editable=False)
+  updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class Book(models.Model):
+  """ This is book model """
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+  title = models.CharField(max_length=50)
+  isbn = models.CharField(max_length=50, unique=True)
+  author=models.ForeignKey(Author,related_name='book_author', on_delete=models.CASCADE)
+  # total_books=models.IntegerField(default=1)
+  category = models.ForeignKey(Category, related_name='book_category', on_delete=models.CASCADE)
+  available = models.BooleanField(default=False)
+  publication_date = models.DateField()
+
+class BorrowRecord(models.Model):
+  id = models.UUIDField(primary_key=True,default=uuid.uuid4(),editable=False)
+  book = models.ForeignKey(Book, related_name='borrow_book',on_delete=models.DO_NOTHING)
+  member=models.ForeignKey(User,related_name='borrow_book_user',on_delete=models.CASCADE)
+  borrow_date = models.DateField(auto_created=True,editable=False)
+  return_date = models.DateField()
--- django/library-management-api/library_management/settings.py
+++ django/library-management-api/library_management/settings.py
@@ -1,15 +1,8 @@
- 
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-rhg6*f)*72r$-e@!8ovr#k+#p*_i4soz$_u9&%o#n)&clo=1&j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
