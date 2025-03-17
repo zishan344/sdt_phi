44aa52173baf3575f7c7e4fdf6334d8b1b53ce62
zishan344 <zishanahmed344@gmail.com>
2025-03-17 09:39:24 +0600

    add djsor and swagger

--- django/library-management-api/library_management/settings.py
+++ django/library-management-api/library_management/settings.py
@@ -20,7 +20,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
+    'djoser',
     'api',
     'books',
     'users'
